f000fa32a88ccb348a7657c4dd73508c8551ce09
Nathan DESNOS <desnos.nathan@gmail.com>
2023-03-04 23:35:36 +0100

    corrections du merge précédent

--- src/entite.hpp
+++ src/entite.hpp
@@ -4,19 +4,11 @@
 #include <iostream>
 
 struct Entite {
-<<<<<<< HEAD
-    int prix = 0;
-    int pv = 1;
-    float position = 0;
-    int taille = 1;
-    bool collision = true;
-=======
 	int id;
 	int prix = 0;
 	int pv = 1;
 	float position = 0;
 	int taille = 1;
->>>>>>> 81766f9d00456237c651e05bb25518a546d0df36
 
 	static int compteur;
 
