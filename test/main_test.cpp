daf69e8c69a9010f230614e98234756bf26bb053
Nathan DESNOS <desnos.nathan@gmail.com>
2023-03-04 16:46:10 +0100

    Test des constructeurs par valeur des unités et des batiments

--- test/main_test.cpp
+++ test/main_test.cpp
@@ -8,29 +8,39 @@
 
 void Test_Entite()
 {
-    printf("[Entite par defaut - debugAfficher]");
+    printf("[Entite par defaut - debugAfficher]\n");
     Entite e;
     e.debugAfficher();
 
-    printf("[Entite par valeurs - debugAfficher]");
+    printf("[Entite par valeurs - debugAfficher]\n");
     Entite e2(10, 50, 3, 2);
     e2.debugAfficher();
+
+    printf("\n");
 }
 
 void Test_Unite()
 {
-    
-    printf("[Unite par defaut - debugAfficher]");
+    printf("[Unite par defaut - debugAfficher]\n");
     Unite u;
     u.debugAfficher();
 
+    printf("[Unite par valeur - debugAfficher]\n");
+    Unite u2(20, 80, 3, 1, 1, 1, 1, 1);
+    u2.debugAfficher();
 
+    printf("\n");
 }
 
 void Test_Batiment()
 {
-    
-    printf("[Batiment par defaut: debugAfficher]");
+    printf("[Batiment par defaut: debugAfficher]\n");
     Batiment b;
     b.debugAfficher();
+
+    printf("[Batiment par valeur: debugAfficher]\n");
+    Batiment b2(100, 300, 10, 1, "mur");
+    b2.debugAfficher();
+
+    printf("\n");
 }
