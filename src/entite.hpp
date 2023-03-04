ff32c3165204256136bae61300ea9b3fcc2d3924
Nathan DESNOS <desnos.nathan@gmail.com>
2023-03-04 15:37:22 +0100

    fixup! création de la classe mère entité pour les propriétés communes aux batiments et aux unités

---
+++ src/entite.hpp
@@ -0,0 +1,19 @@
+#ifndef ENTITE_HPP
+#define ENTITE_HPP
+
+#include <iostream>
+
+struct Entite {
+    int prix = 0;
+    int pv = 1;
+    float position = 0;
+    int taille = 1;
+
+    virtual ~Entite() {};
+
+    virtual void debugAfficher() const {
+        std::printf("Entite: prix [%d] pv [%d], position [%d], taille [%d]\n", prix, pv, position, taille); 
+    }
+};
+
+#endif
