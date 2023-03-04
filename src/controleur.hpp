1c75ef206d93a82ee61b9078753af9e4219584ea
Nathan DESNOS <desnos.nathan@gmail.com>
2023-03-04 22:42:48 +0100

    debut d'implémentation du controlleur du jeu

--- src/controleur.hpp
+++ src/controleur.hpp
@@ -1,9 +1,28 @@
 #pragma once
 #include "joueur.hpp"
+#include "gameLoop.hpp"
 
 class Controleur {
 	Joueur j1, j2;
 
 	void actualisation();
-	void creerUnite(int id);
+	void creerUnite(Joueur joueur, int id);
+};
+
+typedef struct statsUnites {
+	int type;
+	int prix;
+	int pv;
+	int taille;
+	int degatUnite;
+	int degatBatiment;
+	int vitesseAtk;
+	int deplacement;
+	int portee;
+} statsUnites_t;
+
+statsUnites_t tabStatsUnites[] =
+{
+	statsUnites_t {1, 10, 30, 1, 10, 15, 2, 2, 1},
+	statsUnites_t {2, 15, 15, 1, 10, 5, 3, 3, 1}
 };
